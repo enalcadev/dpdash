58fc334df34009327f9cac37e8faf95965216fb2
Enrique Albert Caballero <e.albertcaballero@dxc.com>
2024-05-03 12:08:01 +0200

    DPD-4: fix slash routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,14 +10,14 @@ const {SERVER} = process.env;
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.CI
-		? `https://${BRANCH}.${SERVER}.amplifyapp.com/`
+		? `https://${BRANCH}.${SERVER}.amplifyapp.com`
 		: `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '' : undefined,
 
 	// output: 'server',
 
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	trailingSlash: 'always',
+	// trailingSlash: 'always',
 
 	server: {
 		/* Dev. server only */
